09d85151e9748bff5f253e6dd8039ce384766277
= <seimith@gmail.com>
2019-09-29 08:58:45 -0700

    feat(landing page): Add landing page content; svg; copy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Smith & Sam`,
+    description: `TWO LADIES ENABLING OTHER AWESOME LADIES REACH THEIR POTENTIAL.`,
+    author: `Smith & Sam`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,8 +23,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `custom-theme`,
+        icon: `src/images/smithandsam.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,22 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <center>
+      <h1 style={{ paddingTop: `1em` }}>Hey there, nice to meet you.</h1>
+      <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
+        If you're here, you're probaby an amazing woman who's trying to get stuff done in your life.
+        We get it and we understand you, so don't worry. We're two sisters (Smith and Sam) that are here that want to help you.
+        Combined, we can offer skills that range from administration, lab management, product management, and web development.
+      </p>
+      <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
+        Smith was a User Interface & User Experience Developer, and Sam was an oncology lab manager. <Link to="/about-us/">Learn more about us.</Link>
+      </p>
+    </center>
   </Layout>
 )
 
