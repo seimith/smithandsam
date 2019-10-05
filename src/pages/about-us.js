d0f67cf9d2df850175b90611e56fbe095de2ab6b
= <seimith@gmail.com>
2019-10-05 10:47:43 -0700

    docs(copy update):

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -3,14 +3,30 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeaderMini from "../components/header-mini"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <>
+    <HeaderMini siteTitle='Smith & Sam' />
+    <Layout>
+      <SEO title="About Us" />
+      <center><h1 style={{ paddingTop: `1em`, paddingBottom: `0.5em` }}>So you want to learn more about us...</h1></center>
+
+      <h3>Smith</h3>
+      <p>Smith studied Financial Mathematics, but decided that the investment banking and actuarial world wasn't for her. In fact,
+      her first job was as an Executive Assistant. However, that wasn't as fulfilling, so off she went to a coding bootcamp!</p>
+      <p>After learning how to code, Smith ended up working in Design and Engineering. While working as a
+      developer, Smith taught workshops, spoke at conferences, and began leading teams that comprised of designers, engineers, and quality
+      assurance analysts.
+      </p>
+      <p>Today, Smith still loves development and is deep in the trenches of Design Systems during the day, and by night she's freelancing.</p>
+
+      <h3>Sam</h3>
+      <p>Sam studied Psychology & Biology and spent 6 years in academia working at an Parkinson's Disease research lab. As a lab manager, Sam was
+      in charge of...</p>
+      <Link to="/">Return to the homepage</Link>
+    </Layout>
+  </>
 )
 
 export default SecondPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,47 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Services from "../components/services"
+import Pricing from "../components/pricing"
+import Header from "../components/header"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <center>
-      <h1 style={{ paddingTop: `1em` }}>Hey there, nice to meet you.</h1>
-      <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
-        If you're here, you're probaby an amazing woman who's trying to get stuff done in your life.
-        We get it and we understand you, so don't worry. We're two sisters (Smith and Sam) that are here that want to help you.
-        Combined, we can offer skills that range from administration, lab management, product management, and web development.
-      </p>
-      <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
-        Smith was a User Interface & User Experience Developer, and Sam was an oncology lab manager. <Link to="/about-us/">Learn more about us.</Link>
-      </p>
-    </center>
-  </Layout>
-)
+const IndexPage = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+return (
+  <>
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <center><h1 style={{ paddingTop: `1em`, paddingBottom: `0.5em` }}>Hey there, nice to meet you.</h1></center>
+        <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
+          If you're here, you're probaby an amazing woman who's trying to get stuff done in your life.
+          We get it and we understand you. We're two sisters (Smith and Sam) that want to help you.
+        </p>
+        <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
+          Combined, we can offer skills that range from administration, lab management, product management, and web development.
+        </p>
+        <p style={{ fontWeight: `100`, lineHeight: `2em`, letterSpacing: `0.1em`, fontSize: `0.8em`}}>
+          Smith was a User Interface & User Experience Developer, and Sam was an Research Lab Manager.
+        </p>
+        <p><Link to="/about-us/">Learn more about us.</Link></p>
+      </div>
+    </Layout>
+    <Services />
+    <Pricing />
+  </>
+ )
+}
 
 export default IndexPage
